85336dcac7e921b38934e85be022e24cfa8ca8e2
chuphu2004 <chuphu2004@gmail.com>
2024-05-31 19:50:57 +0700

    [Add] function's details clarify

--- main.c
+++ main.c
@@ -13,13 +13,13 @@ typedef enum Gender Gender;
 };
 typedef struct Date Date; 
 struct Score {
-    float test_mini; //tạo sau
-    float test_45mins;// tạo sau
+    float test_mini[4];
+    float test_45mins[2];
     float mid_term_score;
     float end_term_score;
 };
 typedef struct Score Score;
-
+// viết hàm tính điểm trung bình các môn
 enum Subject_name {
     Maths=1,
     Physics,
@@ -44,7 +44,7 @@ typedef struct Subject Subject;
  struct Student {
     char name[20];
     char class[20];
-    Gender Gender;
+    Gender gender;
     Date birthday;
     Subject subject[12];
 };
@@ -56,26 +56,59 @@ struct Semester {
 };
 typedef struct Semester Semester;
 
+void generate(){
+    //tạo tầm 3 thằng
+}
 
+
+void baomat(){
+    char a[20] = "phuhuy12";
+    char b[20] = "phuhuy12";
+    printf("LOG IN(Use the English keyboard to type)\n");
+    while(1){
+        printf("User name:");
+        char tk[20];
+        scanf("%s", tk);
+        if(strcmp(a, tk)!= 0){
+            printf("Wrong account, please enter again.\n");
+            continue;
+        }
+        else{
+            printf("Password:");
+            char mk[20];
+            scanf("%s", mk);
+            if(strcmp(mk, b)!= 0){
+                printf("Wrong password, please re-enter.\n");
+                continue;
+            }
+            else{
+                break;
+            }
+        }
+    }
+}
 
 void MenuDisplay(){
     printf("Chon chuc nang: \n");
     printf("0. Thoat\n");
-    printf("1. Hien thi danh sach diem: \n");
-    printf("2. Them diem\n");
-    printf("3. Xoa diem\n");
-    printf("4. Sua diem\n");
-    printf("5. \n");
-    printf("6. Tim kiem mon hoc\n");
+    printf("1. Hien thi danh sach diem trung binh: \n");
+    /*extend
+        1.1 theo tên alphab & reverse
+        1.2 theo độ dốc các môn & reverse
+        1.3 theo điểm trung bình & reverse
+    */ 
+    printf("2. Sua diem\n"); 
+    /* lựa chọn sửa điểm
+    2.1 diem mini test 
+    2.2 diem 45p
+    2.3 diem giua ki
+    2.4 diem cuoi ki
+    */ 
+    printf("3. Hien thi danh sach theo hoc sinh\n"); // điền tên học sinh muốn tìm
+    printf("4. Hien thi danh sach theo mon hoc\n"); // điền tên môn học
+
 }
 
 int main(){
-
-    Student nihonjin;
-    nihonjin.subject[0].subject_name = Maths;
-    nihonjin.subject[0].score.test_mini = 0;
-    nihonjin.subject[0].score.test_45mins = 0;
-    nihonjin.subject[0].score.mid_term_score = 0;
-    nihonjin.subject[0].score.end_term_score = 0;
-    printf("%d ",nihonjin.subject[0].subject_name);
+    baomat();
 }

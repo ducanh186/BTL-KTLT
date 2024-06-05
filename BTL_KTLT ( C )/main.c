637ac60966bb2a16cae9b0ebdd2402dcd9e05fd4
chuphu2004 <chuphu2004@gmail.com>
2024-06-05 23:09:42 +0700

    Student alpha bet display

--- BTL_KTLT ( C )/main.c
+++ BTL_KTLT ( C )/main.c
@@ -13,6 +13,33 @@ typedef enum Gender Gender;
     int year;
 };
 typedef struct Date Date; 
+
+struct HoTen {
+    char ho[6];
+    char ten_dem[12];
+    char ten[6];
+};
+typedef struct HoTen HoTen;
+
+float S_Aver(float Arr[],int n){
+    float sum=0;
+    for(int i=0;i<n;i++){
+        sum+=Arr[i];
+    }
+    return sum/n;
+}
+float Aver(float mini[4],float test45[2], float mid, float end){
+    float sum=0;
+    for(int i=0;i<4;i++){
+        sum+=mini[i];
+    }
+    for(int i=0;i<2;i++){
+        sum+=test45[i];
+    }
+    sum+=mid;
+    sum+=end;
+    return sum/13;
+}
 struct Score {
     float test_mini[4];
     float test_45mins[2];
@@ -32,7 +59,7 @@ enum Subject_name {
     Literatures,
     History,
     Geography,
-    Languege,
+    Language,
     Moral_Education,
     Technical,
     PE
@@ -46,7 +73,7 @@ struct Subject {
 typedef struct Subject Subject;
 
  struct Student {
-    char name[20];
+    HoTen hoten;
     char class[20];
     Gender gender;
     Date birthday;
@@ -60,6 +87,24 @@ struct Semester {
     Student student;
 };
 typedef struct Semester Semester;
+
+const char* subjectNameToString(Subject_name subject) {
+    switch (subject) { //// Chuyển đổi Subject_name thành chuỗi
+        case Maths:          return "Toan";
+        case Physics:        return "Ly";
+        case Chemistry:      return "Hoa";
+        case Biology:        return "Sinh";
+        case Informatics:    return "Tin";
+        case Literatures:    return "Van";
+        case History:        return "Su";
+        case Geography:      return "Dia";
+        case Language:       return "Anh";
+        case Moral_Education:return "GDCD";
+        case Technical:      return "CN";
+        case PE:             return "TD";
+        default:             return "Unknown";
+    }
+}
 
 
 void baomat(){
@@ -127,86 +172,184 @@ int Si_so_lop(){
 void Tao_data_lop_6A1(){
   
      Hoc_sinh[0]=(Student){
-        .name = "Nguyen Van A",
+        .hoten = {"Nguyen", "Van", "A"},
         .class = "6A1",
         .gender = Male,
         .birthday = { .day = 20, .month = 5, .year = 2006 },
         .subject = {
             { .subject_name = Maths, .score = { {8.0, 8.5, 7.0, 7.5}, {7.5, 8.0}, 7.8, 8.5 } },
-            {.subject_name = Physics, .score = { {7.0, 6.5, 8.0, 7.2}, {6.8, 7.5}, 7.6, 8.4 } },
+            { .subject_name = Physics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Chemistry, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Biology, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Informatics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Literatures, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = History, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Geography, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Language, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Moral_Education, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Technical, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = PE, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
         }
     };
     Hoc_sinh[1]=(Student){
-        .name = "Tran Thi B",
+        .hoten = {"Tran", "Thi", "B"},
         .class = "6A1",
         .gender = Female,
         .birthday = { .day = 20, .month = 5, .year = 2006 },
         .subject = {
             { .subject_name = Maths, .score = { {8.0, 8.5, 7.0, 7.5}, {7.5, 8.0}, 7.8, 8.5 } },
             { .subject_name = Physics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Chemistry, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Biology, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Informatics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Literatures, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = History, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Geography, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Language, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Moral_Education, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Technical, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = PE, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            
         }
     };
     Hoc_sinh[2]=(Student){
-        .name = "Le Van C",
+        .hoten = {"Le", "Van", "C"},
         .class = "6A1",
         .gender = Male,
         .birthday = { .day = 10, .month = 8, .year = 2006 },
         .subject = {
-            { .subject_name = Maths, .score = { {9.0, 8.5, 7.5, 8.0}, {8.0, 8.5}, 8.3, 9.1 } },
-            { .subject_name = Physics, .score = { {8.0, 7.5, 8.5, 8.2}, {7.8, 8.5}, 8.1, 9.0 } },
+            { .subject_name = Maths, .score = { {8.0, 8.5, 7.0, 7.5}, {7.5, 8.0}, 7.8, 8.5 } },
+            { .subject_name = Physics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Chemistry, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Biology, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Informatics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Literatures, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = History, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Geography, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Language, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Moral_Education, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Technical, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = PE, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
         }
     };
 
     Hoc_sinh[3]=(Student){
-        .name = "Pham Thi D",
+        .hoten = {"Pham", "Thi", "D"},
         .class = "6A1",
         .gender = Female,
         .birthday = { .day = 25, .month = 12, .year = 2006 },
         .subject = {
-            { .subject_name = Maths, .score = { {8.5, 9.0, 8.0, 8.5}, {8.0, 9.0}, 8.5, 9.2 } },
-            { .subject_name = Physics, .score = { {8.0, 7.5, 8.5, 8.7}, {7.8, 8.5}, 8.2, 9.1 } },
+            { .subject_name = Maths, .score = { {8.0, 8.5, 7.0, 7.5}, {7.5, 8.0}, 7.8, 8.5 } },
+            { .subject_name = Physics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Chemistry, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Biology, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Informatics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Literatures, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = History, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Geography, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Language, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Moral_Education, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Technical, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = PE, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
         }
     };
     Hoc_sinh[4]=(Student){
-        .name = "Nguyen Thi E",
+        .hoten = {"Nguyen", "Thi", "E"},
         .class = "6A1",
         .gender = Female,
         .birthday = { .day = 20, .month = 5, .year = 2006 },
         .subject = {
             { .subject_name = Maths, .score = { {8.0, 8.5, 7.0, 7.5}, {7.5, 8.0}, 7.8, 8.5 } },
-            { .subject_name = Physics, .score = { {7.0, 6.5, 8.0, 7.2}, {6.8, 7.5}, 7.6, 8.4 } },
+            { .subject_name = Physics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Chemistry, .score = { {7.5, 7.0, 8.5, 2.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Biology, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Informatics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Literatures, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = History, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Geography, .score = { {7.5, 7.0, 8.5, 7.7}, {9.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Language, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Moral_Education, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 0.9, 2.7 } },
+            { .subject_name = Technical, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = PE, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
         }
     };
-    numStudents=5;
+    Hoc_sinh[5]=(Student){
+        .hoten = {"Nguyen", "Van", "Binh"},
+        .class = "6A1",
+        .gender = Male,
+        .birthday = { .day = 20, .month = 5, .year = 2006 },
+        .subject = {
+            { .subject_name = Maths, .score = { {8.0, 8.5, 7.0, 7.5}, {7.5, 8.0}, 7.8, 8.5 } },
+            { .subject_name = Physics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Chemistry, .score = { {7.5, 7.0, 8.5, 2.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Biology, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Informatics, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Literatures, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = History, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Geography, .score = { {7.5, 7.0, 8.5, 7.7}, {9.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Language, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = Moral_Education, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 0.9, 2.7 } },
+            { .subject_name = Technical, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+            { .subject_name = PE, .score = { {7.5, 7.0, 8.5, 7.7}, {7.3, 8.0}, 7.9, 8.7 } },
+        }
+    };
+    numStudents=6;
 }
 //parameter= tham so => dat ten la para_x :para_Si_so_lop 
 void Hien_thi_ds_Hoc_sinh_Theo_lop(int para_Si_so_lop ){
         printf("|STT");
-        char name[] = "Full Name";
-        printf("|%-20s", name);
-        char lop[] ="Class";
+        char name[] = "Ho va Ten";
+        printf("|%-26s", name);
+        char lop[] ="Lop";
         printf("|%-5s", lop);
-        char gender[] = "Gender";
+        char gender[] = "Gioi Tinh";
         printf("|%-6s",gender);
-        char date[] = "Date of birth";
+        char date[] = "Ngay Sinh";
         printf("|%-13s",date);
-        char maths[] = "Toan";
-        printf("|%-10s",maths);
+        for (int i = 0; i < 12; i++) {
+        printf("|%-4s", subjectNameToString(i + 1)); // Chuyển đổi Subject_name thành chuỗi
+    }
         printf("\n");
     for(int i=0;i<para_Si_so_lop;i++){
         printf("|%-3d",i+1);
-        printf("|%-20s",Hoc_sinh[i].name);// can le tu ben trai sang phai fill 25 ki tu
+        printf("|%-7s%-13s%-6s",Hoc_sinh[i].hoten.ho, Hoc_sinh[i].hoten.ten_dem, Hoc_sinh[i].hoten.ten);// can le tu ben trai sang phai fill 25 ki tu
                 // Ten nguoi thuong ko dai qua 25 ki tu
         printf("|%-5s",Hoc_sinh[i].class);
-        printf("|%-6s",Hoc_sinh[i].gender==1?"Nam":"Nu");
+        printf("|%-9s",Hoc_sinh[i].gender==1?"Nam":"Nu");
         printf("| %-2d",Hoc_sinh[i].birthday.day);
         printf("/%-2d",Hoc_sinh[i].birthday.month);
         printf("/%-5d ",Hoc_sinh[i].birthday.year);
-        printf("|%-3.1f",Hoc_sinh[i].subject[0].score.test_mini[0]);
-        printf("|%-3.1f",Hoc_sinh[i].subject[0].score.test_mini[1]);
-        printf("|%-3.1f\n",Hoc_sinh[i].subject[0].score.test_mini[2]);
+        for (int j=0; j<12; j++){
+        printf("|%-4.1f", Aver(Hoc_sinh[i].subject[j].score.test_mini,Hoc_sinh[i].subject[j].score.test_45mins,
+                                Hoc_sinh[i].subject[j].score.mid_term_score,Hoc_sinh[i].subject[j].score.end_term_score ));
+    }
+    printf("\n");
+}
+}
+
+void Sap_xep_hoc_sinh_theo_ten_AZ(Student* ds, int para_Si_so_lop){
+    int i,j;
+    for (i = 0; i < para_Si_so_lop - 1; i++){
+        for (j = para_Si_so_lop - 1; j > i; j--){
+            if( strcmp(ds[j].hoten.ten, ds[j-1].hoten.ten) < 0){
+                Student sv = ds[j];
+                ds[j] = ds[j-1];
+                ds[j-1] = sv;
+            }
     }
 }
+}
+
+
+
+
+
+void Hien_thi_ds_ten_Hoc_sinh(int para_Si_so_lop){
+        printf("|STT");
+        char name[] = "Ho va Ten";
+        printf("|%-20s", name);
+}
+
 
 int main(){
      Tao_data_lop_6A1();   
@@ -225,6 +368,7 @@ int main(){
 
                 if (sub_Option == 1) {
                     // 1. Theo mac dinh
+                    Sap_xep_hoc_sinh_theo_ten_AZ(Hoc_sinh, numStudents);
                     Hien_thi_ds_Hoc_sinh_Theo_lop(numStudents);
                 } else if (sub_Option == 2) {
                     // Xử lý sắp xếp điểm trung bình
